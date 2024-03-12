c8004e56209767d44615b661c92985dc0b3c3a11
levathian1 <randomynot02@gmail.com>
2024-03-12 10:13:04 +0100

    added necessary components (https://github.com/ssloy/tinyraytracer/wiki/Part-1:-understandable-raytracing#step-3-add-more-spheres)

--- main.cpp
+++ main.cpp
@@ -9,12 +9,21 @@ const int WIDTH = 1024;
 const int HEIGHT = 768;
 const int FOV = M_PI/2;
 
+struct Material {
+    Material(const Vec2f &a, const Vec3f &color, const float &spec) : albedo(a), diffuse_color(color), specular_exponent(spec) {}
+    Material() : albedo(1,0), diffuse_color(), specular_exponent() {}
+    Vec2f albedo;
+    Vec3f diffuse_color;
+    float specular_exponent;
+};
+
 // (0, 0, z) is centered in 2d space
 struct Sphere{
     Vec3f center;
     float radius;
+    Material material;
 
-    Sphere(const Vec3f &c, const float &r) : center(c), radius(r) {}
+    Sphere(const Vec3f &c, const float &r, const Material &m) : center(c), radius(r), material(m) {}
 
     bool ray_intersect(const Vec3f &orig, const Vec3f &dir, float &t0) const {
         Vec3f L = center - orig;
@@ -30,12 +39,20 @@ struct Sphere{
     }
 };
 
+struct Light {
+    Light(const Vec3f &p, const float &i) : position(p), intensity(i) {}
+    Vec3f position;
+    float intensity;
+};
 
 void save_to_file(const char* filename, std::vector<Vec3f> framebuffer){
     std::ofstream ofs; // save the framebuffer to file
     ofs.open("./out.ppm", std::ios::binary);
     ofs << "P6\n" << WIDTH << " " << HEIGHT << "\n255\n";
     for (size_t i = 0; i < HEIGHT*WIDTH; ++i) {
+        Vec3f &c = framebuffer[i];
+        float max = std::max(c[0], std::max(c[1], c[2]));
+        if (max>1) c = c*(1./max);
         for (size_t j = 0; j<3; j++) {
             ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
         }
@@ -43,7 +60,11 @@ void save_to_file(const char* filename, std::vector<Vec3f> framebuffer){
     ofs.close();
 }
 
-bool scene_intersect(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &spheres, Vec3f &hit, Vec3f &N) {
+Vec3f reflect(const Vec3f &I, const Vec3f &N) {
+    return I - N*2.f*(I*N);
+}
+
+bool scene_intersect(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &spheres, Vec3f &hit, Vec3f &N, Material &material) {
     float spheres_dist = std::numeric_limits<float>::max();
     for (size_t i=0; i < spheres.size(); i++) {
         float dist_i;
@@ -51,20 +72,35 @@ bool scene_intersect(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphe
             spheres_dist = dist_i;
             hit = orig + dir*dist_i;
             N = (hit - spheres[i].center).normalize();
+            material = spheres[i].material;
         }
     }
     return spheres_dist<1000;
 }
 
-Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &spheres) {
+Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &spheres, const std::vector<Light> &lights) {
     Vec3f point, N;
-    if (!scene_intersect(orig, dir, spheres, point, N)) {
+    Material material;
+    if (!scene_intersect(orig, dir, spheres, point, N, material)) {
         return Vec3f(0.2, 0.7, 0.8); // background color
     }
-    return Vec3f(0.4, 0.4, 0.3);
+    float diffuse_light_intensity = 0, specular_light_intensity = 0;
+    for (size_t i=0; i<lights.size(); i++) {
+        Vec3f light_dir      = (lights[i].position - point).normalize();
+        float light_distance = (lights[i].position - point).norm();
+
+        Vec3f shadow_orig = light_dir*N < 0 ? point - N*1e-3 : point + N*1e-3; // checking if the point lies in the shadow of the lights[i]
+        Vec3f shadow_pt, shadow_N;
+        Material tmpmaterial;
+        if (scene_intersect(shadow_orig, light_dir, spheres, shadow_pt, shadow_N, tmpmaterial) && (shadow_pt-shadow_orig).norm() < light_distance)
+            continue;
+        diffuse_light_intensity  += lights[i].intensity * std::max(0.f, light_dir*N);
+        specular_light_intensity += powf(std::max(0.f, -reflect(-light_dir, N)*dir), material.specular_exponent)*lights[i].intensity;
+    }
+    return material.diffuse_color * diffuse_light_intensity * material.albedo[0] + Vec3f(1., 1., 1.)*specular_light_intensity * material.albedo[1];
 }
 
-void render(const std::vector<Sphere> &spheres) {
+void render(const std::vector<Sphere> &spheres, const std::vector<Light> &lights) {
     std::vector<Vec3f> framebuffer(WIDTH*HEIGHT);
 
     for (size_t j = 0; j<HEIGHT; j++) {
@@ -72,7 +108,7 @@ void render(const std::vector<Sphere> &spheres) {
             float x =  (2*(i + 0.5)/(float)WIDTH - 1)*tan(FOV/2.)*WIDTH/(float)HEIGHT;
             float y = -(2*(j + 0.5)/(float)HEIGHT - 1)*tan(FOV/2.);
             Vec3f dir = Vec3f(x, y, -1).normalize();
-            framebuffer[i+j*WIDTH] = cast_ray(Vec3f(0,0,0), dir, spheres);
+            framebuffer[i+j*WIDTH] = cast_ray(Vec3f(0,0,0), dir, spheres, lights);
         }
     }
 
@@ -80,16 +116,24 @@ void render(const std::vector<Sphere> &spheres) {
 }
 
 int main() {
-    Sphere sphere1(Vec3f(0, -3, -16), 2);
-    Sphere sphere2(Vec3f(0, 0, -16), 2);
-    Sphere sphere3(Vec3f(0, 3, -16), 2);
+    Material ivory(Vec2f(0.6,  0.3), Vec3f(0.4, 0.4, 0.3),   50.);
+    Material red_rubber(Vec2f(0.9,  0.1), Vec3f(0.3, 0.1, 0.1),   10.);
+
+    Sphere sphere1(Vec3f(0, -3, -16), 2, ivory);
+    Sphere sphere2(Vec3f(0, 0, -16), 2, ivory);
+    Sphere sphere3(Vec3f(0, 3, -16), 2, red_rubber);
 
     std::vector<Sphere> spheres;
+    std::vector<Light>  lights;
 
     spheres.push_back(sphere1);
     spheres.push_back(sphere2);
     spheres.push_back(sphere3);
 
-    render(spheres);
+    lights.push_back(Light(Vec3f(-20, 20,  20), 1.5));
+    lights.push_back(Light(Vec3f( 30, 50, -25), 1.8));
+    lights.push_back(Light(Vec3f( 30, 20,  30), 1.7));
+
+    render(spheres, lights);
     return 0;
 }
