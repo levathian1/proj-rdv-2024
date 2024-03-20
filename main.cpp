91348fd20981ce3a46ee6c7edc134603ccd01dcf
levathian1 <randomynot02@gmail.com>
2024-03-20 15:31:13 +0100

    added small snow effect

--- main.cpp
+++ main.cpp
@@ -97,9 +97,91 @@ struct Light {
     float intensity;
 };
 
+float interpolate(float a0, float a1, float w) {
+    // You may want clamping by inserting:
+     if (0.0 > w) return a0;
+     if (1.0 < w) return a1;
+
+    return (a1 - a0) * w + a0;
+    /* // Use this cubic interpolation [[Smoothstep]] instead, for a smooth appearance:
+     * return (a1 - a0) * (3.0 - w * 2.0) * w * w + a0;
+     *
+     * // Use [[Smootherstep]] for an even smoother result with a second derivative equal to zero on boundaries:
+     * return (a1 - a0) * ((w * (w * 6.0 - 15.0) + 10.0) * w * w * w) + a0;
+     */
+}
+
+typedef struct {
+    float x, y;
+} vector2;
+
+/* Create pseudorandom direction vector
+ */
+vector2 randomGradient(int ix, int iy) {
+    // No precomputed gradients mean this works for any number of grid coordinates
+    const unsigned w = 8 * sizeof(unsigned);
+    const unsigned s = w / 2; // rotation width
+    unsigned a = ix, b = iy;
+    a *= 3284157443; b ^= a << s | a >> w-s;
+    b *= 1911520717; a ^= b << s | b >> w-s;
+    a *= 2048419325;
+    float random = a * (3.14159265 / ~(~0u >> 1)); // in [0, 2*Pi]
+    vector2 v;
+    v.x = cos(random); v.y = sin(random);
+    return v;
+}
+
+// Computes the dot product of the distance and gradient vectors.
+float dotGridGradient(int ix, int iy, float x, float y) {
+    // Get gradient from integer coordinates
+    vector2 gradient = randomGradient(ix, iy);
+
+    // Compute the distance vector
+    float dx = x - (float)ix;
+    float dy = y - (float)iy;
+
+    // Compute the dot-product
+    return (dx*gradient.x + dy*gradient.y);
+}
+
+// Compute Perlin noise at coordinates x, y
+float noise(float x, float y) {
+    // Determine grid cell coordinates
+    int x0 = (int)floor(x);
+    int x1 = x0 + 1;
+    int y0 = (int)floor(y);
+    int y1 = y0 + 1;
+
+    
+
+    // Determine interpolation weights
+    // Could also use higher order polynomial/s-curve here
+    // Fixing values here instead of original to force positive values at all times
+    float sx = 0.4;
+    float sy = 0.3;
+
+    // std::cout << x << " " << x0 << "\n";
+
+    // Interpolate between grid point gradients
+    float n0, n1, ix0, ix1, value;
+
+    n0 = dotGridGradient(x0, y0, x, y);
+    n1 = dotGridGradient(x1, y0, x, y);
+    ix0 = interpolate(n0, n1, sx);
+
+    n0 = dotGridGradient(x0, y1, x, y);
+    n1 = dotGridGradient(x1, y1, x, y);
+    ix1 = interpolate(n0, n1, sx);
+
+    value = interpolate(ix0, ix1, sy);
+    return value; // Will return in range -1 to 1. To make it in range 0 to 1, multiply by 0.5 and add 0.5
+}
+
+
+
 void save_to_file(const char* filename, std::vector<Vec3f> framebuffer){
     std::ofstream ofs; // save the framebuffer to file
-    ofs.open("./out.ppm", std::ios::binary);
+    ofs.open(filename, std::ios::binary);
     ofs << "P6\n" << WIDTH << " " << HEIGHT << "\n255\n";
     for (size_t i = 0; i < HEIGHT*WIDTH; ++i) {
         Vec3f &c = framebuffer[i];
@@ -183,6 +265,11 @@ Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &s
 
 void render(const std::vector<Sphere> &spheres, const std::vector<Cone> &cones, const std::vector<Light> &lights) {
     std::vector<Vec3f> framebuffer(WIDTH*HEIGHT);
+    std::vector<Vec3f> perlin(WIDTH*HEIGHT*3);
+
+
+    std::cout << "print";
+
 
     for (size_t j = 0; j<HEIGHT; j++) {
         for (size_t i = 0; i<WIDTH; i++) {
@@ -202,8 +289,29 @@ void render(const std::vector<Sphere> &spheres, const std::vector<Cone> &cones, 
         if (max>1) c = c*(1./max);
         for (size_t j = 0; j<3; j++) {
             pixmap[i*3+j] = (unsigned char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
+            // pixmap[i*3+j] = (unsigned char)(0);
         }
     }
+
+    for (size_t i = 0; i < HEIGHT*WIDTH; ++i) {
+        Vec3f &c = framebuffer[i];
+        float max = std::max(c[0], std::max(c[1], c[2]));
+        if (max>1) c = c*(1./max);
+        for (size_t j = 0; j<3; j++) {
+            // pixmap[i*3+j] = (unsigned char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
+            float noise_val = noise(i, j);
+            // TODO: look into pixel discoloration at the end 
+            if(noise_val > 0.5){
+                pixmap[i*3+j] = 255;
+                pixmap[i*3+j+1] = 255;
+                pixmap[i*3+j+2] = 255;
+                perlin[i+j] = Vec3f(255, 255, 255);
+            }
+        }
+
+    }
+
+    save_to_file("pog.ppm", perlin);
     stbi_write_jpg("out.jpg", WIDTH, HEIGHT, 3, pixmap.data(), 100);
 }
 
